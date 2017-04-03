0717b9927d36ba26393ce8a602c74b4ac1bf8b75
upam <upam>
2017-04-03 10:13:39 -0500

    plugins
    plugin de camera, device, network

--- js/action.js
+++ js/action.js
@@ -19,17 +19,87 @@ var fn={
 	document.addEventListener("deviceready",fn.init, false);
 	},
 	init:function(){
-		alert("hola mundo");
-		navigator.notification.beep(2);
+	$('#sig1').tap(fn.almacenar);
+    $('#calc').tap(fn.calcular);
+    $('#enviar').tap(fn.enviar);
+    $('#dis1').tap(fn.dispositivo);
+    $('#red1').tap(fn.estaConectado);
+    $('#regFoto').tap(fn.tomarFoto);
+	},
+    almacenar:function(){
+     navigator.vibrate(1000);
+     navigator.notification.beep(1);
 
-	navigator.notification.alert(
-    'You are the winner!',  // message
-    null,         // callback
-    'Game Over',            // title
-    'Done'                  // buttonName
-);
-	}
+     var nombre_usuario=$('#name').val();
+     localStorage.setItem("nombre",nombre_usuario);
+     
+     navigator.notification.alert("hola "+nombre_usuario,null,"Bienvenido","ok");
+    },
+    calcular:function(){
+        var peso=$('#peso').val();
+        var altura=$('#altura').val();
+        var imc=parseFloat(peso)/(parseFloat(altura)*parseFloat(altura));
 
+        var nom=localStorage.getItem('nombre');
+        localStorage.setItem("imc",imc);
+
+    navigator.notification.alert("hola "+nom+ "tu imc es"+imc,
+        null,"IMC","ok");
+
+    },
+    enviar:function(){
+        var peso=$('#peso').val();
+        var altura=$('#altura').val();
+        var imc=localStorage.getItem("imc");
+        var nombre=localStorage.getItem("nombre");
+        alert(nombre+" "+imc);
+
+        if(peso!='' && altura!='' && nombre!='' && imc!='')
+        {
+            $.ajax({
+            type:"POST",
+            url:"http://uprrimc.azurewebsites.net/test2.php",
+            data:"peso="+peso+"&altura="+altura+"&nombre="nombre+"&imc"+imc
+                  }).done(function(msg){
+                    if(msg==1)
+                    {
+                        alert("datos enviados y recibidos");
+                    }
+                    else {alert("Error en el registro"); }
+
+             });
+
+        }else{ alert("Todos los datos son requeridos");}
+
+    },
+    dispositivo:function(){
+        $('#data').append('<p>'+device.model+'</p>');
+        $('#data').append('<p>'+device.platform+'</p>');
+        $('#data').append('<p>'+device.version+'</p>')
+
+    },
+    estaConectado:function{
+        navigator.notification.beep(1);
+        var conn=navigator.connection.type;
+
+        if(conn!=Connection.NONE)
+            alert("conectado");
+        else
+            alert("desconectado");
+    },
+    tomarFoto:function{
+        // start image capture
+    navigator.device.capture.captureImage(function(img){
+        for(i=0;i<img.length;i++){
+            ruta=img[i].fullPath;
+        }
+        $('#regFoto').attr('foto',ruta);
+        $('#regImg').html('<img src="'+ruta+'" width="100%"/>');
+    },
+    function(err){alert("error"+err.code)}, 
+     {limit:2});
+
+    }
 }
 
 $(fn.ready);
