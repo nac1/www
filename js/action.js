349d8356f4aacd287e76bd9655c1bfe8c4e16009
upam <upam>
2017-04-06 15:55:09 -0500

    code
    qr code

--- js/action.js
+++ js/action.js
@@ -24,7 +24,7 @@ var fn={
 	$('#sig1').tap(fn.almacenar);
     $('#calc').tap(fn.calcular);
     $('#envia').tap(fn.envia);
-    $('#dis1').tap(fn.dispositivo);
+    $('#dis1').tap(fn.code);//dispositivo);
     $('#red1').tap(fn.estaconectado);
     $('#regFoto').tap(fn.tomarFoto);
 	},
@@ -106,6 +106,31 @@ var fn={
     function(err){alert("error"+err.code)}, 
      {limit:2});
 
+    },
+    code:function(){
+        cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : true, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: true, // Android, launch with the torch switched on (if available)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS
+      }
+   ); 
     }
 }
 
